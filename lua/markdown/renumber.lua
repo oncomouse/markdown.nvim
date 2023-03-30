486fdae567af23a20fed75f4553aaae0f39fad2c
oncomouse <oncomouse@gmail.com>
2023-03-30 18:38:57 -0500

    initial renumbering seems to work

--- lua/markdown/renumber.lua
+++ lua/markdown/renumber.lua
@@ -1,11 +1,6 @@
 local M = {}
 
-function detect_indent(row)
-	local line = vim.api.nvim_buf_get_lines(0, row, row + 1 , false)[1]
-	local spaces = string.match(line, "^(%s*)")
-	if not spaces then
-		return
-	end
+function detect_block(row)
 	local i = row - 2
 	while i >= 0 do
 		local l = vim.api.nvim_buf_get_lines(0, i, i + 1, false)
@@ -13,14 +8,10 @@ function detect_indent(row)
 			break
 		end
 		l = l[1]
-		local s = string.match(l, "^(%s*)")
-		if s == nil then break end
-		if s == nil or #s ~= #spaces then
-			break
-		end
+		if #l == 0 then break end
 		i = i - 1
 	end
-	local start = i + 2
+	local start = i + 1
 	i = row + 1
 	while i >= 1 do
 		local l = vim.api.nvim_buf_get_lines(0, i, i + 1, false)
@@ -28,27 +19,32 @@ function detect_indent(row)
 			break
 		end
 		l = l[1]
-		local s = string.match(l, "^(%s*)")
-		if s == nil or #s ~= #spaces then
-			break
-		end
+		if #l == 0 then break end
 		i = i + 1
 	end
 	local stop = i - 1
-	return start - 1, stop - 1
+	return start, stop
 end
 
 function M.renumber_ordered_list()
 	local row = require("markdown.utils").get_current_row()
-	local start, stop = detect_indent(row)
+	local start, stop = detect_block(row)
 	local lines = vim.api.nvim_buf_get_lines(0, start, stop + 1, false)
-	local line_number = 1
-	for num, line in ipairs(lines) do
-		local spaces, contents = line:match("^(%s*)%d+([.] .*$)")
-		if spaces ~= nil then
-			line_number = line_number + 1
+	local levels = {}
+	local output = {}
+	for _, line in ipairs(lines) do
+		local spaces, contents = line:match("^(%s*)%d+([.] .*)")
+		if spaces == nil then
+			table.insert(output, line)
+		else
+			if not levels[spaces] then
+				levels[spaces] = 1
+			end
+			table.insert(output, string.format("%s%d%s", spaces, levels[spaces], contents))
+			levels[spaces] = levels[spaces] + 1
 		end
 	end
+	vim.api.nvim_buf_set_lines(0, start, stop + 1, false, output)
 end
 
 return require("markdown.utils").add_key_bindings(M, {})
