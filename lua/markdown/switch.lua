dcd20204db714cad938fe6ef39d6be92422b612e
oncomouse <oncomouse@gmail.com>
2023-04-10 16:05:32 -0500

    feat: operatorfunc mode

--- lua/markdown/switch.lua
+++ lua/markdown/switch.lua
@@ -26,9 +26,7 @@ function M.switch_line()
 	vim.api.nvim_win_set_cursor(0, { start + 1, col })
 end
 
-function M.switch()
-	local row = require("markdown.utils").get_current_row()
-	local start, stop = require("markdown.utils").detect_block(row)
+function switch_lines(start, stop)
 	local lines = vim.api.nvim_buf_get_lines(0, start, stop + 1, false)
 	local output = {}
 	for _, line in ipairs(lines) do
@@ -38,7 +36,23 @@ function M.switch()
 	require("markdown.renumber").renumber_ordered_list()
 end
 
+function M.switch()
+	local row = require("markdown.utils").get_current_row()
+	local start, stop = require("markdown.utils").detect_block(row)
+	switch_lines(start, stop)
+end
+
+function M.switch_opfunc()
+	local start, _ = unpack(vim.api.nvim_buf_get_mark(0, "["))
+	local stop, _ = unpack(vim.api.nvim_buf_get_mark(0, "]"))
+	if start == 0 or stop == 0 then return end
+	start = start - 1
+	stop = stop - 1
+	switch_lines(start, stop)
+end
+
 return require("markdown.utils").add_key_bindings(M, {
-	{ "n", "<Plug>(markdown-nvim-switch)", "<cmd>lua MarkdownNvim.switch()<CR>", "<leader>ms" },
+	{ "n", "<Plug>(markdown-nvim-switch)", "<cmd>lua MarkdownNvim.switch()<CR>", "<leader>mss" },
+	{ "n", "<Plug>(markdown-nvim-switch_opfunc)", "<cmd>set opfunc=v:lua.MarkdownNvim.switch_opfunc<CR>g@", "<leader>ms" },
 	{ "i", "<Plug>(markdown-nvim-switch)", "<cmd>lua MarkdownNvim.switch_line()<CR>", "<C-Z>" },
 })
