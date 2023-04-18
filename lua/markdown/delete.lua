f116e2b46e38ed76a7c536768fb78013016859b5
oncomouse <oncomouse@gmail.com>
2023-04-18 11:25:20 -0500

    feat: visual delete

--- lua/markdown/delete.lua
+++ lua/markdown/delete.lua
@@ -16,11 +16,20 @@ function M.delete_opfunc(mode)
 		row_left, col_left = unpack(vim.api.nvim_buf_get_mark(0, "["))
 		row_right, col_right = unpack(vim.api.nvim_buf_get_mark(0, "]"))
 	elseif mode == "visual" then
+		register = vim.v.register
 		row_left, col_left = unpack(vim.api.nvim_buf_get_mark(0, "<"))
 		row_right, col_right = unpack(vim.api.nvim_buf_get_mark(0, ">"))
 	end
 	row_left = row_left - 1
 	row_right = row_right - 1
+	-- Linewise visual delete:
+	if col_left == 0 and col_right == vim.v.maxcol then
+		local deleted = vim.api.nvim_buf_get_lines(0, row_left, row_right + 1, false)
+		vim.fn.setreg(register, table.concat(deleted, "\n"), "l")
+		vim.api.nvim_buf_set_lines(0, row_left, row_right + 1, false, {})
+		vim.cmd([[exec "normal! \<Plug>(markdown-nvim-renumber)"]])
+		return
+	end
 	local output = {}
 	if row_left == row_right then
 		local line = vim.api.nvim_buf_get_lines(0, row_left, row_left + 1, false)[1]
@@ -39,23 +48,27 @@ function M.delete_opfunc(mode)
 		deleted[#deleted] = deleted[#deleted]:sub(1, col_right + 1)
 		vim.fn.setreg(register, table.concat(deleted, "\n"))
 
-		table.insert(output, start_line)
-		table.insert(output, stop_line)
+		if #start_line > 0 then
+			table.insert(output, start_line)
+		end
+		if #stop_line > 0 then
+			table.insert(output, stop_line)
+		end
 	end
 	vim.api.nvim_buf_set_lines(0, row_left, row_right + 1, false, output)
 	vim.cmd([[exec "normal! \<Plug>(markdown-nvim-renumber)"]])
 end
 
-function M.delete_line()
-	local row = require("markdown.utils").get_current_row()
+function M.delete_line(row)
+	row = row or require("markdown.utils").get_current_row()
 	local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
-	vim.fn.setreg(vim.v.register, line)
+	vim.fn.setreg(vim.v.register, line, "l")
 	vim.api.nvim_buf_set_lines(0, row, row + 1, false, {})
 end
 
 return require("markdown.utils").add_key_bindings(M, {
 	{ "n", "<Plug>(markdown-nvim-delete-line)", [[<cmd>lua MarkdownNvim.delete_line()<CR><cmd>exec "normal! \<Plug>(markdown-nvim-renumber)"<CR>]], "dd" },
 	{ "n", "<Plug>(markdown-nvim-delete-opfunc)", "<cmd>lua MarkdownNvim.delete_opfunc()<CR>g@", "d" },
-	-- { "v", "<Plug>(markdown-nvim-delete-visual)", ":<C-u>lua MarkdownNvim.delete_opfunc('visual')<CR>", "d" },
+	{ "v", "<Plug>(markdown-nvim-delete-visual)", ":<C-u>lua MarkdownNvim.delete_opfunc('visual')<CR>", "d" },
 })
 
