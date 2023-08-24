2321a7f6607e9b1532f9436f8ae84233f55b0ed8
oncomouse <oncomouse@gmail.com>
2023-08-24 15:07:29 -0500

    fix: fixes a problem with incorrect list type detection

--- lua/markdown/newline.lua
+++ lua/markdown/newline.lua
@@ -5,7 +5,7 @@ function M.insert_newline(above)
 	local action = above and "O" or "o"
 	local line = vim.api.nvim_get_current_line()
 	local lnum = require("markdown.utils").get_current_row()
-	local other_line = vim.api.nvim_buf_get_lines(
+	local next_line = vim.api.nvim_buf_get_lines(
 		0,
 		lnum + (above and -1 or 1),
 		lnum + (above and 0 or 2),
@@ -14,20 +14,20 @@ function M.insert_newline(above)
 	if line:match("^> ") then
 		return action .. "> "
 	end
-	if other_line and other_line:match("^> ") then
+	if next_line and next_line:match("^> ") then
 		return action .. "> "
 	end
 	-- Lists, ordered and unordered
 	local space, number = line:match("^(%s*)(%d+)[.] ")
-	if not space and other_line then
-		space, number = other_line:match("^(%s*)(%d+)[.] ")
+	if not space and next_line then
+		space, _ = next_line:match("^(%s*)(%d+)[.] ")
 	end
-	if space then
-		return string.format("%s%d<Home><Esc><C-A>A. <Esc>A", action, number)
+	if space and number then
+		return string.format("%s%d. <Esc>%sA", action, number, require("markdown.renumber").trigger)
 	end
 	space, number = line:match("^(%s*)(" .. list_chars .. ") ")
-	if not space and other_line then
-		space, number = other_line:match("^(%s*)(" .. list_chars .. ") ")
+	if not space and next_line then
+		space, number = next_line:match("^(%s*)(" .. list_chars .. ") ")
 	end
 	if space then
 		return string.format("%s%s ", action, number)
